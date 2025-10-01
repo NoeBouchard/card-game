9b7d05624c610c77b3a2e20e92405616c4dc268f
NoeBouchard <noyouchka.bouchard@gmail.com>
2025-10-01 15:17:17 +0100

    started the first classes and structure of the card game

---
+++ CardGame.java
@@ -0,0 +1,11 @@
+public class CardGame {
+    // prompt user in terminal for the number of players
+
+    // creates the pack of cards using method in Card.java
+
+
+    // then shuffle the pack, distribute to the player's hands (4 cards each) in round robin fashion
+
+    // then distribute the rest of the cards in the same fashion into n different decks (where n is the number of players)
+    
+}
---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+
+    // player has a hand of cards (array of 4 cards)
+    private Card[] hand;
+
+    // nested class that decides hich card to discard anduse deck class to draw and discard cards into/out of hand
+
+    // method to check whether the player has won the game (4 cards of the same number)
+
+    
+}
