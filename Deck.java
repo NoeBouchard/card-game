7a80cf8d561274983cdb467a4938c5e66dcb98b1
NoeBouchard <noyouchka.bouchard@gmail.com>
2025-10-01 15:17:24 +0100

    same

---
+++ Deck.java
@@ -0,0 +1,6 @@
+public class Deck {
+    // deck has a queue of cards
+    // method to add a card to the bottom of the queue
+    // method to remove a card from the top of the queue
+    // both methods has to happen in concurrency (use thread)
+}
