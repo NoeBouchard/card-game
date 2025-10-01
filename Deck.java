130b8fc77bb1330808be2eeaef85c14f7faa6996
NoeBouchard <noyouchka.bouchard@gmail.com>
2025-10-01 19:33:47 +0100

    fixed deck creation

--- Deck.java
+++ Deck.java
@@ -1,19 +1,19 @@
-import java.util.Queue;
+import java.util.*;
 
 public class Deck {
     // deck has a queue of cards
-    private Queue<Card> deck;
+    private final Queue<Card> deck = new LinkedList<>();
     // method to add a card to the bottom of the queue
-    public createDeck() {
-        deck = new LinkedList<>();
+    public void  createDeck() {
+        deck.clear();
     }
     
     public void addCardToBottom(Card card) {
-        deck.add(card);
+        deck.offer(card);
     }
     // method to remove a card from the top of the queue
     public Card removeCardFromTop() {
-        return deck.remove();
+        return deck.poll();
     }
     // both methods has to happen in concurrency (use thread)
 }
